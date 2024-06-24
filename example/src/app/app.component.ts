3a4fbb7b3edb49885be87722c8d0c47ead3615f7
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-06-24 14:44:05 +0200

    implemented package in example app

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MidiParser } from '@bechstein/midi-parser'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly midiParser = new MidiParser({
+    logMessages: true,
+    enableHighResVelocity: false
+  });
+
+  constructor() {
+    this.midiParser.subscribe((message) => console.log(message));
+    navigator.requestMIDIAccess()
+      .then((access) => {
+        access.inputs.forEach((input) => {
+          input.onmidimessage = (message) => this.midiParser.parseMessage(message)
+        })
+      })
+  }
+}
