c24b47b8172e5acf7dc135b56a40e981a804377b
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-10-10 16:35:39 +0200

    Adds capacitor to example app

--- example/src/app/app.config.ts
+++ example/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideIonicAngular({})],
 };
