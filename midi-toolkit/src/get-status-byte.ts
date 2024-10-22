c66f17be7310f32a27ff222e7d2336eab15c654c
Ehsan Barooni <66495214+ebarooni@users.noreply.github.com>
2024-10-22 13:28:31 +0200

    Throws error if provided length of data is less than 1

--- midi-toolkit/src/get-status-byte.ts
+++ midi-toolkit/src/get-status-byte.ts
@@ -1,3 +1,6 @@
 export function getStatusByte(data: Uint8Array): number {
+  if (data.length < 1) {
+    throw new Error('Invalid MIDI data: data length must be at least 1');
+  }
   return data[0];
 }
